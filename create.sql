1628313ed4f1a0a8606b9e1951f6f387fbfe90f4
kae1dy <ilyamorozov2003@mail.ru>
2024-03-01 20:28:33 +0300

    edit

--- create.sql
+++ create.sql
@@ -1,17 +1,11 @@
 DROP SCHEMA IF EXISTS billing CASCADE;
 CREATE SCHEMA billing;
 
--- Счета клиентов
+-- Счета клиентов:
 -- Баланс
--- Поступления на счет
--- Списания за оказание услуг связи
--- Ограничения: размер максимального кредита и сроки его погашения
-
-DROP TYPE IF EXISTS client_type;
-CREATE TYPE client_type AS ENUM (
-                        'Физ. лицо', 
-                        'Юр. лицо'
-);
+-- История поступлений/списаний
+-- Размер максимального кредита
+-- Сроки его погашения
 
 CREATE TABLE billing.accounts
 (
@@ -22,32 +16,54 @@ CREATE TABLE billing.accounts
 	acc_terms date DEFAULT NULL
 );
 
--- Клиенты – физические лица и организации
--- Наименование или ФИО
--- Контакты: контактные лица, адрес(а), телефон(ы), e-mail(ы)
+-- Организации:
+-- Название
+-- 1-ая запись - "Физ. лицо"
+
+CREATE TABLE billing.organizations
+(
+	org_id bigserial PRIMARY KEY,
+	org_name text NOT NULL,
+	org_inn char(10)
+);
+
+-- Клиенты:
+-- Относится ли клиент к организации (если нет, то внешний ключ ведёт на 1-ую запись в таблице "billing.organization", где стоит "Физ. лицо")
+-- ФИО
+-- Информация о клиенте: телефон, email, адрес
 -- История услуг: услуги, в какое время оказывались
+-- Внешний ключ на счёт клиента
 
 CREATE TABLE billing.clients
 (
     client_id bigserial PRIMARY KEY,
-	client_type client_type,
+	client_org bigint DEFAULT 1 REFERENCES billing.organizations ON DELETE CASCADE,
     client_name text NOT NULL,
 	client_info jsonb,
-	client_services jsonb,
 	-- 1-to-1
 	acc_id bigint DEFAULT NULL UNIQUE REFERENCES billing.accounts ON DELETE SET NULL
 );
 
--- Услуги
+-- Услуги:
 -- Наименование
--- Характеристики: номер, группа номеров, Интернет, SMS, спец. предложения
--- Тарифный план (какая часть услуги в какое время сколько будет стоить)
+-- Характеристики пакета
+-- Тарифный план
 
 CREATE TABLE billing.services 
 (   
-    service_code char(8) UNIQUE NOT NULL,
+	service_id bigserial PRIMARY KEY,
     service_name text NOT NULL,
 	service_package jsonb,
 	service_tariff jsonb
 );
 
+-- Many-to-Many
+CREATE TABLE billing.client_service
+(   
+    client_id bigint REFERENCES billing.clients ON DELETE CASCADE,
+    service_id bigint REFERENCES billing.services,
+	contract_num text NOT NULL,
+	contract_begin date NOT NULL,
+	contract_end date DEFAULT NULL CHECK(contract_begin <= contract_end),
+    CONSTRAINT client_service_pkey PRIMARY KEY (client_id, service_id)
+);
--- insert.sql
+++ insert.sql
@@ -1,28 +1,39 @@
-INSERT INTO billing.services(service_code, service_name, service_package, service_tariff) VALUES
-	('KBKWNXW7', 'Всё за 500', '{"min": 300, "sms": 500, "internet": 800, "max_members": 1}', '{"tariff": 500.0, "extra_min": 1.5, "extra_sms": 1.0, "extra_internet": 5.0}'),
-	('4RGP5JL6', 'Всё за 1000', '{"min": 500, "sms": 1000, "internet": 1500, "max_members": 1}', '{"tariff": 1000.0, "extra_min": 1.5, "extra_sms": 1.0, "extra_internet": 5.0}'),
-	('089G7FGP', 'Семья', '{"min": 1500, "sms": 2000, "internet": 2000, "max_members": 3}', '{"tariff": 1200.0, "extra_min": 1.5, "extra_sms": 1.0, "extra_internet": 5.0}'),
-	('X7DAVJLL', 'Семья+', '{"min": 2500, "sms": 2500, "internet": 3000, "max_members": 5}', '{"tariff": 1700.0, "extra_min": 1.5, "extra_sms": 1.0, "extra_internet": 5.0}'),
-	('CKJ6KV4I', 'Только звонки', '{"min": 500, "sms": 100, "internet": 0, "max_members": 1}', '{"tariff": 150.0, "extra_min": 1.5, "extra_sms": 1.0, "extra_internet": 5.0}'),
-	('WBRNXG5E', 'Только интернет', '{"min": 0, "smhs": 100, "internet": 1000, "max_members": 1}', '{"tariff": 200.0, "extra_min": 1.5, "extra_sms": 1.0, "extra_internet": 5.0}'),
-	('E3GJLLLS', 'Поминутный', '{"min": 0, "sms": 0, "internet": 0, "max_members": 1}', '{"tariff": 0.0, "extra_min": 0.5, "extra_sms": 1.0, "extra_internet": 1.5}');
+INSERT INTO billing.organizations(org_name, org_inn) VALUES 
+	('Физ. лицо', NULL),
+	('ИП Ромашка', '3620961634'),
+	('ОАО РЖД', '9777241723'),
+	('ПАО Газпром', '5474983662'),
+	('ООО Советник', '5287317415');
+	
+
+INSERT INTO billing.services(service_name, service_package, service_tariff) VALUES
+	('Всё за 500', '{"min": 300, "sms": 500, "internet": 800, "max_members": 1}', '{"tariff": 500.0, "extra_min": 1.5, "extra_sms": 1.0, "extra_internet": 5.0}'),
+	('Всё за 1000', '{"min": 500, "sms": 1000, "internet": 1500, "max_members": 1}', '{"tariff": 1000.0, "extra_min": 1.5, "extra_sms": 1.0, "extra_internet": 5.0}'),
+	('Семья', '{"min": 1500, "sms": 2000, "internet": 2000, "max_members": 3}', '{"tariff": 1200.0, "extra_min": 1.5, "extra_sms": 1.0, "extra_internet": 5.0}'),
+	('Семья+', '{"min": 2500, "sms": 2500, "internet": 3000, "max_members": 5}', '{"tariff": 1700.0, "extra_min": 1.5, "extra_sms": 1.0, "extra_internet": 5.0}'),
+	('Только звонки', '{"min": 500, "sms": 100, "internet": 0, "max_members": 1}', '{"tariff": 150.0, "extra_min": 1.5, "extra_sms": 1.0, "extra_internet": 5.0}'),
+	('Только интернет', '{"min": 0, "smhs": 100, "internet": 1000, "max_members": 1}', '{"tariff": 200.0, "extra_min": 1.5, "extra_sms": 1.0, "extra_internet": 5.0}'),
+	('Поминутный', '{"min": 0, "sms": 0, "internet": 0, "max_members": 1}', '{"tariff": 0.0, "extra_min": 0.5, "extra_sms": 1.0, "extra_internet": 1.5}');
+	
 	
 INSERT INTO billing.accounts(acc_balance, acc_history, acc_credit, acc_terms) VALUES
-	(2557, '[1575, -76, 958, -590, 690]', 300, '2024-03-26'),
-	(975, '[1648, -989, 779, -653, 190]', 100, '2024-06-05'),
-	(165, '[1370, -679, 143, -509, 741, -901]', 400, '2024-07-09'),
-	(710, '[1229, -572, 69, -114, 912, -814]', 1500, '2024-07-07'),
-	(793, '[1371, -883, 845, -540]', 1000, '2024-06-22'),
-	(1608, '[1870, -149, 145, -477, 963, -744]', 300, '2024-04-21'),
-	(1483, '[1586, -631, 528]', 1000, '2024-08-08'),
-	(495, '[1089, 278, -233, 194, -833]', 500, '2024-06-21'),
-	(890, '[954, -998, 893, -504, 747, -202]', 500, '2024-05-11'),
-	(1515, '[1838, -323]', 100, '2024-06-21');
+	(2557, '{"2024-01-21": 1575, "2024-01-21": -76, "2024-01-21": 958, "2024-01-21": -590, "2024-01-21": 690}', 300, '2024-03-26'),
+	(975, '{"2024-01-21": 1648, "2024-01-21": -989, "2024-01-21": 779, "2024-01-21": -653, "2024-01-21": 190}', 100, '2024-06-05'),
+	(165, '{"2024-01-21": 1370, "2024-01-21": -679, "2024-01-21": 143, "2024-01-21": -509, "2024-01-21": 741, "2024-01-22": -901}', 400, '2024-07-09'),
+	(1483, '{"2024-01-21": 1586, "2024-01-21": -631, "2024-01-21": 528}', 1000, '2024-08-08'),
+	(1515, '{"2024-01-21": 1838, "2024-01-21": -323}', 100, '2024-06-21');
+	
+INSERT INTO billing.clients(client_org, client_name, client_info, acc_id) VALUES
+	(1, 'Абрамов Михаил Андреевич', '{"phone": "89021417351", "email": "abram@mail.ru", "address": "Москва, ул. Пушкинская, д. 15"}', 1),
+	(2, 'Валявкин Максим Александрович', '{"phone": "89021417352", "email": "val@mail.ru", "address": "Калуга, ул. Гурьянова, д. 50"}', 2),
+	(3, 'Кабанов Иван Юрьевич', '{"phone": "89021417353", "email": "kaban@mail.ru", "address": "Ярославль, ул. Мирная, д. 14"}', 3),
+	(4, 'Митрофанов Алексей Антонович', '{"phone": "89021417354", "email": "mitrofan@mail.ru", "address": "Москва, пр. Ломоносовский, д. 17"}', 4),
+	(5, 'Артамонова Юлия Максимовна', '{"phone": "89021417355", "email": "yulik@mail.ru", "address": "Ульяновск, ул. Промышленная, д. 4"}', 5);
 	
 	
-INSERT INTO billing.clients(client_type, client_name, client_info, client_services, acc_id) VALUES
-	('Физ. лицо', 'Абрамов Михаил Андреевич', '{"Абрамов Михаил Андреевич": {"phone": "89021417351", "email": "abram@mail.ru", "address": "Москва, ул. Пушкинская, д. 15"}}', '{"KBKWNXW7": {"begin": "2024-01-21", "end": "2024-02-21"}}', 1),
-	('Физ. лицо', 'Валявкин Максим Александрович', '{"Валявкин Максим Александрович": {"phone": "89021417352", "email": "val@mail.ru", "address": "Калуга, ул. Гурьянова, д. 50"}}', '{"E3GJLLLS": {"begin": "2023-12-15", "end": "2024-02-15"}}', 2),
-	('Физ. лицо', 'Кабанов Иван Юрьевич', '{"Кабанов Иван Юрьевич": {"phone": "89021417353", "email": "kaban@mail.ru", "address": "Ярославль, ул. Мирная, д. 14"}}', '{"WBRNXG5E": {"begin": "2022-01-07", "end": "2024-01-07"}}', 3),
-	('Физ. лицо', 'Митрофанов Алексей Антонович', '{"Митрофанов Алексей Антонович": {"phone": "89021417354", "email": "mitrofan@mail.ru", "address": "Москва, пр. Ломоносовский, д. 17"}}', '{"089G7FGP": {"begin": "2023-05-24", "end": "2023-08-24"}}', 4),
-	('Физ. лицо', 'Артамонова Юлия Максимовна', '{"Артамонова Юлия Максимовна": {"phone": "89021417355", "email": "yulik@mail.ru", "address": "Ульяновск, ул. Промышленная, д. 4"}}', '{"KBKWNXW7": {"begin": "2024-01-21", "end": "2023-01-21"}}', 5);
+INSERT INTO billing.client_service(client_id, service_id, contract_num, contract_begin) VALUES
+	(1, 1, '1-24/01', '2024-01-21'),
+	(2, 2, '1-23/05', '2023-05-23'),
+	(3, 3, '1-24/02', '2024-02-10'),
+	(4, 4, '2-24/02', '2024-02-05'),
+	(5, 5, '3-24/02', '2024-02-11');
